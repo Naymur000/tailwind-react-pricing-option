4040eda4b38af320f71f0725f8fc43aea116f199
Naymur000 <nayeemur000@gmail.com>
2025-11-22 23:08:40 +0600

    3rd commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,48 +1,60 @@
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Link from "./Link";
+import { useState } from "react";
 
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Players",
+    path: "/players",
+  },
+  {
+    id: 3,
+    name: "Teams",
+    path: "/teams",
+  },
+  {
+    id: 4,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const links = navLinks.map((route) => <Link key={route.id} route={route} />);
 
 const NavBar = () => {
-  const navLinks = [
-    {
-      id: 1,
-      name: "Home",
-      path: "/",
-    },
-    {
-      id: 2,
-      name: "Players",
-      path: "/players",
-    },
-    {
-      id: 3,
-      name: "Teams",
-      path: "/teams",
-    },
-    {
-      id: 4,
-      name: "About",
-      path: "/about",
-    },
-    {
-      id: 5,
-      name: "Contact",
-      path: "/contact",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   return (
     <nav className="flex justify-between mx-10">
-      <Menu></Menu>
-      <h1>My Navbar</h1>
+      <span className="flex" onClick={()=> setOpen(!open)} >
+        
+        <ul>
+          {
+            links
+          }
+        </ul>
+
+        <h1>My Navbar</h1>
+      </span>
 
       <ul className="flex">
-        {navLinks.map((route) => (
-          <Link route={route}></Link>
-        ))}
+        {
+          links
+        }
       </ul>
-      
-      <button>Sign in</button>
+
+      <h1>Sign In</h1>
     </nav>
   );
 };
